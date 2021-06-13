c5d3a1f042aba97a3e0d65f98dab88317fa7bd06
Rabkov Evgeniy <rabkov_evgeniy@mail.ru>
2021-06-13 15:35:25 +0700

    Refactoring:  Make method Print() extention

---
+++ Navigator/GraphExtention.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Navigator;
+
+namespace GraphExtention
+{
+    static class GraphExtention
+    {
+        /// <summary>
+        /// Выводит информацию о графе в виде матрицы смежности.
+        /// </summary>
+        public static void Print(this Graph graph)
+        {
+            Console.WriteLine("Матрица смежности:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\t");
+            for (int i = 0; i < graph.CitysNames.Length; i++)
+            {
+                Console.Write(graph.CitysNames[i] + "\t");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < graph.AdjacencyMatrix.GetLength(0); i++)
+            {
+                Console.Write(graph.CitysNames[i] + "\t");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                for (int j = 0; j < graph.AdjacencyMatrix.GetLength(1); j++)
+                {
+                    if (graph.AdjacencyMatrix[i, j] == -1)
+                        Console.Write("-\t");
+                    else
+                        Console.Write(graph.AdjacencyMatrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+}
